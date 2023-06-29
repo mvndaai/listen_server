d4a7ffe39637fc70e12f02592d83e252b696afed
Jason Mavandi <mvndaai@gmail.com>
2023-06-29 13:52:37 -0600

    Upload local files

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/mvndaai/listen_server
+
+go 1.20
---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8888", nil)
+}
+
+type printable struct {
+	Path    string      `json:"path"`
+	Method  string      `json:"method"`
+	Body    string      `json:"body,omitempty"`
+	Headers http.Header `json:"headers,omitempty"`
+}
+
+func handler(w http.ResponseWriter, req *http.Request) {
+	p := printable{
+		Path:    req.URL.Path,
+		Method:  req.Method,
+		Headers: req.Header,
+	}
+
+	body, err := io.ReadAll(req.Body)
+	defer req.Body.Close()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	if len(body) > 0 {
+		p.Body = string(body)
+	}
+
+	b, err := json.MarshalIndent(p, "", "\t")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println(string(b))
+}
